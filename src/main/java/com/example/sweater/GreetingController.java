b8b06f5b3c70358db91e2a87a2dada25c3d8283f
Kenayo <swordscream@yandex.ru>
2018-11-15 21:16:06 +0300

    Added BD

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -1,12 +1,20 @@
 package com.example.sweater;
 
+import com.example.sweater.repos.MessageRepo;
+import com.example.sweater.domain.Message;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepo messagesRepo;
+
     @GetMapping("/greeting")
     public String greeting(
             @RequestParam(name="name", required=false, defaultValue="World") String name,
@@ -18,7 +26,19 @@ public class GreetingController {
 
     @GetMapping
     public String main(Map<String, Object> model) {
-        model.put("some", "Hello, asshole");
+        Iterable<Message> messages = messagesRepo.findAll();
+
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        messagesRepo.save(message);
+        Iterable<Message> messages = messagesRepo.findAll();
+        model.put("messages", messages);
         return "main";
     }
 }
